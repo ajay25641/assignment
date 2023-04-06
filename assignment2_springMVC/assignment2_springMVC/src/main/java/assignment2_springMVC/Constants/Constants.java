42a44d08a2e8aa2107664ed2405967d23456b8c7
ajay kumar <ajay25641@gmail.com>
2023-04-06 17:14:55 +0530

    updated at 06/04/2023

--- assignment2_springMVC/assignment2_springMVC/src/main/java/assignment2_springMVC/Constants/Constants.java
+++ assignment2_springMVC/assignment2_springMVC/src/main/java/assignment2_springMVC/Constants/Constants.java
@@ -3,6 +3,6 @@ package assignment2_springMVC.Constants;
 public class Constants {
 	public static final String DB_URL="jdbc:postgresql://localhost/testdb?escapeSyntaxCallMode=call";
     public static final String USER_NAME="postgres";
-    public static final String PASSWORD="Rajaryan.25641";
+    public static final String PASSWORD="";
 
 }
--- assignment2_springboot/src/main/java/com/example/assignment2/Constants/Constants.java
+++ assignment2_springboot/src/main/java/com/example/assignment2/Constants/Constants.java
@@ -3,5 +3,5 @@ package com.example.assignment2.Constants;
 public class Constants {
     public static final String DB_URL="jdbc:postgresql://localhost/testdb?escapeSyntaxCallMode=call";
     public static final String USER_NAME="postgres";
-    public static final String PASSWORD="Rajaryan.25641";
+    public static final String PASSWORD="";
 }
